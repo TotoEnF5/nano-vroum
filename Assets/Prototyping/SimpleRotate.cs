c9b100f21673930f1c7c5d3d33a339ffd384e3ed
Gorglomux <aralleaume@gmail.com>
2025-12-04 19:24:51 +0100

    Turbines

---
+++ Assets/Prototyping/SimpleRotate.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class SimpleRotate : MonoBehaviour
+{
+    public AnimationCurve CurveRotate;
+    public float speed = 1;
+    //lerps from 0-1 with the curveRotate 
+    [SerializeField]
+    private float m_rotationTime = 0;
+    private Vector3 startingRotation;
+    [Range(0,360)]
+    public float AngleIncrement = 360f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        m_rotationTime = m_rotationTime + Time.deltaTime * speed;
+        
+        float rotationStrength = CurveRotate.Evaluate(m_rotationTime);
+        transform.eulerAngles = Vector3.Lerp(startingRotation, startingRotation + new Vector3(0, 0, AngleIncrement), CurveRotate.Evaluate(m_rotationTime));
+        if (m_rotationTime >= 1)
+        {
+            m_rotationTime = 0;
+            startingRotation = transform.eulerAngles;
+        }
+    }
+}
