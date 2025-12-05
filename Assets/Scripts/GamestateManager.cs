3112799e37fd705d4e40cb58a42bed6b9fd97d5c
TotoEnF5 <totooodev@proton.me>
2025-12-05 17:04:50 +0100

    Remove GameButtonHandler.cs, move functionality to GamestateManager

--- Assets/Scripts/GamestateManager.cs
+++ Assets/Scripts/GamestateManager.cs
@@ -1,6 +1,8 @@
 using System;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public enum Gamestate
 {
@@ -15,6 +17,29 @@ public class GamestateManager : MonoBehaviour
     public static Transform CurrentCheckpoint;
     
     private static Gamestate _gamestate = Gamestate.Playing;
+    
+    private InputAction _pauseAction;
+
+    private void Awake()
+    {
+        _pauseAction = InputSystem.actions.FindAction("Pause");
+    }
+    
+    private void Update()
+    {
+        if (_pauseAction.triggered)
+        {
+            Scene pauseScene = SceneManager.GetSceneByName("MenuPause");
+            if (pauseScene.isLoaded)
+            {
+                SceneManager.UnloadSceneAsync(pauseScene);
+            }
+            else
+            {
+                SceneManager.LoadScene("MenuPause", LoadSceneMode.Additive);
+            }
+        }
+    }
 
     public static void SetGamestate(Gamestate state)
     {
