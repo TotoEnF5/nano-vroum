b1425f7c4c269bbcb1cf3af0b42085b4be423029
TotoEnF5 <totooodev@proton.me>
2025-12-05 10:08:14 +0100

    Add button handler

---
+++ Assets/Scripts/Pause Menu/ButtonHandler.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public Button backToGame;
+    public Button settings;
+    public Button backToMainMenu;
+    public Button quitGame;
+
+    public void Awake()
+    {
+        backToGame.onClick.AddListener(OnBackToGame);
+        settings.onClick.AddListener(OnSettings);
+        backToMainMenu.onClick.AddListener(OnBackToMainMenu);
+        quitGame.onClick.AddListener(OnQuitGame);
+    }
+    
+    private void OnBackToGame()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private void OnSettings()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private void OnBackToMainMenu()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private void OnQuitGame()
+    {
+        Application.Quit();
+    }
+}
